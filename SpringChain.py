5c32817eac02b2d1965a196a025d2af9ad247803
Sriram Loganathan <76449821+ShadowAsura@users.noreply.github.com>
2023-08-24 19:55:30 -0700

    SpringChain Update
    - Updated SpringChain
    - Springs affect connected chains from both directions now

--- SpringChain.py
+++ SpringChain.py
@@ -9,17 +9,20 @@ class SpringChain:
         self.dragging = False
         self.dragged_spring = None
     def update(self):
-        # If the last spring is being dragged, update its end position to the mouse position
-        if self.dragging:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            self.springs[-1].end = [mouse_x, mouse_y]
-
-        # Update each spring in the chain
         for i, spring in enumerate(self.springs):
             spring.update()
-            # If it's not the last spring, set the anchor of the next spring to the end of the current spring
-            if i < len(self.springs) - 1:
-                self.springs[i + 1].anchor = spring.end.copy()
+            
+            # If it's not the first spring, adjust its anchor based on the end of the spring above it
+            if i > 0:
+                spring.anchor[0] = self.springs[i-1].end[0]
+                spring.anchor[1] = self.springs[i-1].end[1]
+            
+            # Check if the spring's length exceeds a threshold (e.g., 1.5 times its rest length)
+            if math.dist(spring.anchor, spring.end) > 1.5 * spring.rest_length and i > 0:
+                # Adjust the end position of the spring above
+                self.springs[i-1].end[0] += (spring.end[0] - spring.anchor[0]) * 0.1
+                self.springs[i-1].end[1] += (spring.end[1] - spring.anchor[1]) * 0.1
+
 
 
 
