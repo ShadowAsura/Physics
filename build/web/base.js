a2d8560011e98d4553a59a1e8286397e41514a78
Sriram Loganathan <76449821+ShadowAsura@users.noreply.github.com>
2025-03-09 15:21:56 -0700

    Update base.js

--- build/web/base.js
+++ build/web/base.js
@@ -1,5 +1,9 @@
 // Setting up the scene, camera, and renderer
 // Setting up the scene, camera, and renderer
+
+import * as THREE from 'three';
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
